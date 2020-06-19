55b171c97fd606d7189bc1a5769da6300352fa44
Hiroki Yaginuma <56863616+hirokiyaginuma@users.noreply.github.com>
2020-06-19 17:42:31 -0500

    commit

--- flask_project/__init__.py
+++ flask_project/__init__.py
@@ -3,8 +3,6 @@ import os
 from flask import Flask
 
 def create_app(test_config=None):
-
-    print(str(sys.path))
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
